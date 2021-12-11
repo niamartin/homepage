304d38ac11f805f81d6117b9acdf3052555e9305
Nia Martin <71298008+niamartin@users.noreply.github.com>
2021-12-11 17:53:15 -0500

    added collection and nav

--- scripts.js
+++ scripts.js
@@ -2,3 +2,20 @@
 
 console.log('Welcome to my personal homepage!')
 
+/* OFFSCREEN MENU */
+function nav(tgl = 'toggle' , cls = 'nav-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+
+// function navclose (tgl = 'toggle', cls = 'nav-inactive') { if (document.querySelector(`.${tgl}`)) {
+//     document.querySelector(`.${tgl}`).addEventListener('click', 
+//         () => { document.body.classList.toggle(cls) }
+//     )
+// }
+// }
+nav()
+navclose()

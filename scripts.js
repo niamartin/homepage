208b1d1ca0055927a7f6ac47b38bd4bdec0718e7
Nia Martin <71298008+niamartin@users.noreply.github.com>
2021-12-15 05:09:48 -0500

    menu added , toggles added

--- scripts.js
+++ scripts.js
@@ -10,12 +10,73 @@ function nav(tgl = 'toggle' , cls = 'nav-active') {
         )
     }
 } 
-
-// function navclose (tgl = 'toggle', cls = 'nav-inactive') { if (document.querySelector(`.${tgl}`)) {
-//     document.querySelector(`.${tgl}`).addEventListener('click', 
-//         () => { document.body.classList.toggle(cls) }
-//     )
-// }
-// }
 nav()
-navclose()
+function designs(tgl = 'des-enter' , cls = 'designs-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+designs()
+function motion(tgl = 'mot-enter' , cls = 'motion-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+motion()
+function photo(tgl = 'photo-enter' , cls = 'photo-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+photo()
+function back(tgl = 'port-back' , cls = 'designs-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+back()
+function back2(tgl = 'port-back' , cls = 'motion-active') {
+    if (document.querySelector(`.${tgl}`)) {
+        document.querySelector(`.${tgl}`).addEventListener('click', 
+            () => { document.body.classList.toggle(cls) }
+        )
+    }
+} 
+
+back2()
+// FORWARDS CLICKS
+
+function toggle(clickTarget, toggleTarget, toggleClass) {
+	if (document.querySelector(clickTarget) && document.querySelector(toggleTarget)) {
+		let ct = document.querySelector(clickTarget)
+		let tt = document.querySelector(toggleTarget)
+		let tc = toggleClass.replace('.', '')
+		ct.addEventListener('click', () => {
+			tt.classList.toggle(tc)
+		})
+	} else { 
+		if (!document.querySelector(clickTarget) && !document.querySelector(toggleTarget)) {
+		console.error(`unable to find ${clickTarget} and ${toggleTarget}, check your code`)
+		} else if (!document.querySelector(clickTarget)) {
+			console.error(`unable to find ${clickTarget}`)
+		} else {
+			console.error(`unable to find ${toggleTarget}`)
+		}
+	}
+	
+}
+
+
+
+
+
+
+
